90287dee8c78fe8b3a0f8b4c86d905ff7ad8b602
Bhavik Goyal <goyal.bhavik28@gmail.com>
2025-05-11 19:59:14 +0530

    Initial Commit

---
+++ cameraApp/cameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraAppApp.swift
+//  cameraApp
+//
+//  Created by Bhavik Goyal on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct cameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
